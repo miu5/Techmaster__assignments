17c6600eec3c7531343ab060ea4d5e2b931b0161
miu5 <97599561+miu5@users.noreply.github.com>
2022-02-17 15:51:44 +0700

    initialize

--- RNC/src/RNC.java
+++ RNC/src/RNC.java
@@ -1,12 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class RNC {
         public static void main(String[] args){
 
             Random rd = new Random();
+            Scanner sc = new Scanner(System.in);
 
-            int x = rd.nextInt();
-            System.out.println(x);
+            int a = rd.nextInt(100);//Random một số.
+            System.out.println("Nhập một số ngẫu nhiên: ");
+            int b = sc.nextInt();
+            System.out.println("Số của chương trình là: "+a);
+
+            String c = (a > b) ? a+" lớn hơn "+b : a+" nhỏ hơn "+b;
+            System.out.println(c);
+
         }
     }
-    
+
