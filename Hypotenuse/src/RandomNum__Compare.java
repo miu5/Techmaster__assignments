f14a5372cb85305dc8002617803f4ed2bd5858ba
miu5 <97599561+miu5@users.noreply.github.com>
2022-02-17 15:41:13 +0700

    upload

---
+++ Hypotenuse/src/RandomNum__Compare.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class RandomNum__Compare {
+    public static void main(String[] args){
+
+        Random rd = new Random();
+
+        int x = rd.nextInt();
+        System.out.println(x);
+    }
+}
---
+++ RNC/src/RNC.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class RNC {
+        public static void main(String[] args){
+
+            Random rd = new Random();
+
+            int x = rd.nextInt();
+            System.out.println(x);
+        }
+    }
+    
